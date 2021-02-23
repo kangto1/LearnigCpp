c11114f423f08c91a989b6b405dd9e1fbaa0ddc9
totokangto <79521384+totokangto@users.noreply.github.com>
2021-02-24 00:04:00 +0900

    2월 24일

--- ex01.cpp
+++ ex01.cpp
@@ -1,8 +1,16 @@
+//비교연산자
 #include <stdio.h>
 
 int main() {
-	printf("동해물과 백두산이\n");
-	printf("마르고\n");
-	printf("닳도록\n");
+	int a, b;
+	scanf_s("%d%d", &a, &b);
 	
+	// bool : 진리값을 저장하는 변수
+	bool p = a > b;
+	bool q = a < b;
+	bool r = a == b; // == : 같다
+	
+	printf("%d\n", p);
+	printf("%d\n", q);
+	printf("%d\n", r);
 }
--- ex02.cpp
+++ ex02.cpp
@@ -1,6 +1,15 @@
+//논리연산자(그리고, 또는 . . .)
+// && || !
 #include <stdio.h>
 int main() {
-	int a = 2;
-	int b = 3;
-	printf("%d + %d = %d\n", a, b, a + b);
+	int a;
+	scanf_s("%d", &a);
+
+	int p = a >= 1 && a <= 10;
+	int q = a == 3 || a == 7;
+	int r = !q;
+
+	printf("%d\n", p);
+	printf("%d\n", q);
+	printf("%d\n", r);
 }
